088d9b3971fdd7611ab9d83db61fd07a2a310a91
Nonso <nonsootoh@gmail.com>
2022-10-07 15:38:26 -0400

    fix: styling for delete comment

--- int-comments-app/src/components/Comment.tsx
+++ int-comments-app/src/components/Comment.tsx
@@ -1,17 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "../styles/Comps/Comp.css";
 
-import { MdReply } from "react-icons/md";
+import { MdReply, MdEdit, MdDelete } from "react-icons/md";
 
 import ProfileImg from "../Imgs/avatars/image-amyrobson.png";
 
 const Comment: FC = () => {
+  const [commentRating, setCommentRating] = useState<number>(12);
+
+  const onIncrement = () => {
+    setCommentRating((prev) => prev + 1);
+  };
+
+  const onDecrement = () => {
+    if (commentRating > 0) {
+      setCommentRating((prev) => prev - 1);
+    }
+  };
   return (
     <section className="commentCon">
       <div className="rating">
-        <p className="rating__Rate">+</p>
-        <p className="rating__number">12</p>
-        <p className="rating__Rate">-</p>
+        <p className="rating__Rate" onClick={() => onIncrement()}>
+          +
+        </p>
+        <p className="rating__number">{commentRating}</p>
+        <p className="rating__Rate" onClick={() => onDecrement()}>
+          -
+        </p>
       </div>
 
       <div className="comment">
@@ -30,8 +45,23 @@ const Comment: FC = () => {
       </div>
 
       <div className="options">
-        <MdReply />
-        <span className="replyBtn__text">Reply</span>
+        {true ? (
+          <div className="UserOptions">
+            <div className="UserOptions__Delete">
+              <MdDelete className="UserOptions__Delete--Icon" />
+              <span className="UserOptions__Delete--Text">Delete</span>
+            </div>
+            <div className="UserOptions__Edit">
+              <MdEdit className="UserOptions__Delete--Icon" />
+              <span className="UserOptions__Delete--Text">Edit</span>
+            </div>
+          </div>
+        ) : (
+          <div className="replyBtn">
+            <MdReply />
+            <span className="replyBtn__text">Reply</span>
+          </div>
+        )}
       </div>
     </section>
   );
