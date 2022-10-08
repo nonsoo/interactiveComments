6c7249501d9686753081b710c8e4bd8a45ada377
Nonso <nonsootoh@gmail.com>
2022-10-07 21:56:15 -0400

    feat: adds replybox person

--- int-comments-app/src/components/Comment.tsx
+++ int-comments-app/src/components/Comment.tsx
@@ -4,6 +4,8 @@ import "../styles/Comps/Comp.css";
 
 import { MdReply, MdEdit, MdDelete } from "react-icons/md";
 
+import ProfImg from "../Imgs/avatars/image-amyrobson.png";
+
 const Comment: FC<CommentProp> = ({
   userName,
   userImg,
@@ -14,6 +16,16 @@ const Comment: FC<CommentProp> = ({
 }) => {
   const [commentRating, setCommentRating] = useState<number>(rating);
   const [userImgImport, setUserImgImport] = useState<any>("");
+  const [replyMessage, setReplyMessage] = useState<string>("");
+  const [replyStatus, setReplyStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (userImg) {
+      import(`../Imgs/avatars/${userImg}`)
+        .then((res) => setUserImgImport(res.default))
+        .catch((err) => console.error(err));
+    }
+  }, [userImg]);
 
   const onIncrement = () => {
     setCommentRating((prev) => prev + 1);
@@ -24,57 +36,69 @@ const Comment: FC<CommentProp> = ({
       setCommentRating((prev) => prev - 1);
     }
   };
-
-  useEffect(() => {
-    if (userImg) {
-      import(`../Imgs/avatars/${userImg}`)
-        .then((res) => setUserImgImport(res.default))
-        .catch((err) => console.error(err));
-    }
-  }, [userImg]);
+  const onSubmitForm = (e: any) => {
+    e.preventDefault();
+  };
 
   return (
-    <section className="commentCon">
-      <div className="rating">
-        <p className="rating__Rate" onClick={() => onIncrement()}>
-          +
-        </p>
-        <p className="rating__number">{commentRating}</p>
-        <p className="rating__Rate" onClick={() => onDecrement()}>
-          -
-        </p>
-      </div>
-
-      <div className="comment">
-        <div className="commentUser">
-          <img src={userImgImport} alt="" className="commentUser__Img" />
-          <p className="commentUser__Name">{userName}</p>
-          <p className="commentUser__day">{postDate}</p>
+    <>
+      <section className="commentCon">
+        <div className="rating">
+          <p className="rating__Rate" onClick={() => onIncrement()}>
+            +
+          </p>
+          <p className="rating__number">{commentRating}</p>
+          <p className="rating__Rate" onClick={() => onDecrement()}>
+            -
+          </p>
         </div>
 
-        <p className="comment__Comment">{content}</p>
-      </div>
+        <div className="comment">
+          <div className="commentUser">
+            <img src={userImgImport} alt="" className="commentUser__Img" />
+            <p className="commentUser__Name">{userName}</p>
+            {myComment && <p className="commentUser__currUser">you</p>}
+            <p className="commentUser__day">{postDate}</p>
+          </div>
 
-      <div className="options">
-        {myComment ? (
-          <div className="UserOptions">
-            <div className="UserOptions__Delete">
-              <MdDelete className="UserOptions__Delete--Icon" />
-              <span className="UserOptions__Delete--Text">Delete</span>
+          <p className="comment__Comment">{content}</p>
+        </div>
+
+        <div className="options">
+          {myComment ? (
+            <div className="UserOptions">
+              <div className="UserOptions__Delete">
+                <MdDelete className="UserOptions__Delete--Icon" />
+                <span className="UserOptions__Delete--Text">Delete</span>
+              </div>
+              <div className="UserOptions__Edit">
+                <MdEdit className="UserOptions__Delete--Icon" />
+                <span className="UserOptions__Delete--Text">Edit</span>
+              </div>
             </div>
-            <div className="UserOptions__Edit">
-              <MdEdit className="UserOptions__Delete--Icon" />
-              <span className="UserOptions__Delete--Text">Edit</span>
+          ) : (
+            <div className="replyBtn" onClick={() => setReplyStatus(true)}>
+              <MdReply />
+              <span className="replyBtn__text">Reply</span>
             </div>
-          </div>
-        ) : (
-          <div className="replyBtn">
-            <MdReply />
-            <span className="replyBtn__text">Reply</span>
-          </div>
-        )}
-      </div>
-    </section>
+          )}
+        </div>
+      </section>
+
+      {replyStatus && (
+        <form className="replyBox" onSubmit={onSubmitForm}>
+          <img src={ProfImg} alt="myProfilePic" className="replyBox__Img" />
+          <textarea
+            className="replyBox__TextArea"
+            value={replyMessage}
+            onChange={(e) => {
+              setReplyMessage(e.target.value);
+            }}
+          />
+          <input type="submit" className="replyBox__Submit" value="Reply" />
+        </form>
+      )}
+    </>
   );
 };
 
